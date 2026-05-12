247b465ec1da7207520ccb4e1ff7002032d22a81
mattes <veynoplugins@gmail.com>
2026-05-12 09:07:41 +0200

    Define global Constants in Constants.java

---
+++ common/src/main/java/de/tankblast/Constants.java
@@ -0,0 +1,13 @@
+package de.tankblast;
+
+public class Constants {
+
+//    Max Units a Player Can move per second
+    public static final double MOVEMENT_SPEED = 3;
+
+    public static final Version VERSION = Version.V0_0_1;
+
+//    Max degrees a Player can rotate per second
+    public static final double MAX_ROTATION_SPEED = 90;
+
+}
