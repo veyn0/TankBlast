2166989d4255d57e6d5f45c567700666b4a33043
mattes <veynoplugins@gmail.com>
2026-05-12 12:38:23 +0200

    Add Method Stub to PhysicsService.java

--- client/src/main/java/de/tankblast/game/PhysicsService.java
+++ client/src/main/java/de/tankblast/game/PhysicsService.java
@@ -1,4 +1,16 @@
 package de.tankblast.game;
 
+import de.tankblast.model.entity.Entity;
+import de.tankblast.model.geometry.Vector;
+
 public class PhysicsService {
+
+
+//     TODO: Should Differentiate between Players and Bullets which would need to deflect upon colliding with another entity.
+    public static Vector computeNextValidPosition(Entity entity, World world, double distance){
+        return null;
+    }
+
+
+
 }
