991537f595dc0bc92da08798485bade2223811e8
mattes <veynoplugins@gmail.com>
2026-05-19 12:15:03 +0200

    implement rendering in VoxelRenderer.java

--- client/src/main/java/de/tankblast/render/Voxel.java
+++ client/src/main/java/de/tankblast/render/Voxel.java
@@ -5,4 +5,31 @@ public class Voxel {
     private int colour;
     private double x, y, size, rotation;
 
+    public Voxel(int colour, double x, double y, double size, double rotation) {
+        this.colour = colour;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.rotation = rotation;
+    }
+
+    public int getColour() {
+        return colour;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
 }
--- client/src/main/java/de/tankblast/render/VoxelRenderer.java
+++ client/src/main/java/de/tankblast/render/VoxelRenderer.java
@@ -1,5 +1,8 @@
 package de.tankblast.render;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,7 @@ public class VoxelRenderer implements Renderer{
     private List<Voxel> voxels = new ArrayList<>();
     private Camera camera;
 
+    private int backgroundColour;
 
     @Override
     public void clear(int colour) {
@@ -42,6 +46,38 @@ public class VoxelRenderer implements Renderer{
 
     @Override
     public BufferedImage render(int width, int height) {
-        return null;
+        synchronized (lock) {
+            BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = result.createGraphics();
+            try {
+                g.setColor(new Color(backgroundColour));
+                g.fillRect(0, 0, width, height);
+
+                double pixelPerUnit = height / (2.0 * camera.getPosition().getZ());
+                double cameraX = camera.getPosition().getX();
+                double cameraY = camera.getPosition().getY();
+
+                for (Voxel v : voxels) {
+                    double screenX = width  / 2.0 + (v.getX() - cameraX) * pixelPerUnit;
+                    double screenY = height / 2.0 - (v.getY() - cameraY) * pixelPerUnit;
+                    double screenSize = v.getSize() * pixelPerUnit;
+
+                    g.setColor(new Color(v.getColour()));
+
+                    if (v.getRotation() != 0.0) {
+                        AffineTransform old = g.getTransform();
+                        g.rotate(v.getRotation(), screenX, screenY);
+                        g.fill(new Rectangle2D.Double(screenX - screenSize /2, screenY - screenSize /2, screenSize, screenSize));
+                        g.setTransform(old);
+                    } else {
+                        g.fill(new Rectangle2D.Double(screenX - screenSize /2, screenY - screenSize /2, screenSize, screenSize));
+                    }
+                }
+            } finally {
+                g.dispose();
+            }
+            return result;
+        }
     }
+
 }
