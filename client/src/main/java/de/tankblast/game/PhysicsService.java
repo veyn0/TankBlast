3fa185f5321912464a11be4dc183577647369997
mattes <veynoplugins@gmail.com>
2026-05-19 09:15:17 +0200

    create Rendering class and Method outline

--- client/src/main/java/de/tankblast/game/PhysicsService.java
+++ client/src/main/java/de/tankblast/game/PhysicsService.java
@@ -7,7 +7,7 @@ public class PhysicsService {
 
 
 //     TODO: Should Differentiate between Players and Bullets which would need to deflect upon colliding with another entity.
-    public static Vector computeNextValidPosition(Entity entity, World world, double distance){
+    public static Vector computeNextValidPosition(Entity entity, World world, double timeNanos){
         return null;
     }
 
---
+++ client/src/main/java/de/tankblast/render/BufferedImageDisplay.java
@@ -0,0 +1,9 @@
+package de.tankblast.render;
+
+import java.awt.image.BufferedImage;
+
+public interface BufferedImageDisplay {
+
+    void showImage(BufferedImage bufferedImage);
+
+}
